ae42fbdeab941fce710bae2f133615a80cd72cdc
fernandovidalpiazza <fervidalpiazza@gmail.com>
2024-04-24 17:44:50 -0300

    logrado

--- cart/cartmanger.js
+++ cart/cartmanger.js
@@ -1,24 +1,23 @@
-import path from 'path'
+import path from 'path';
 import config from '../config.js';
-import fs from 'fs'
-
-
+import fs from 'fs';
 
 const carritoFilePath = path.join(config.DIRNAME, './cart/addtocart.json');
+const productFilePath = path.join(config.DIRNAME, './product.json');
 
-const cartsMananger = {
-
-  getAllCarts: (req,res) => {
+const cartsManager = {
+  getAllCarts: (req, res) => {
     try {
-      const carts = JSON.parse(fs.readFileSync(carritoFilePath, 'utf-8'))
-      res.json(carts)      
-  }catch (error) {
-    res.status(500).json({ error: 'Error al obtener los carritos' });
-  }},
+      const carts = JSON.parse(fs.readFileSync(carritoFilePath, 'utf-8'));
+      res.json(carts);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al obtener los carritos' });
+    }
+  },
 
   createCart: (req, res) => {
-    const carts = JSON.parse(fs.readFileSync(carritoFilePath, 'utf-8'));
     try {
+      const carts = JSON.parse(fs.readFileSync(carritoFilePath, 'utf-8'));
       const newCart = {
         id: carts.length + 1,
         products: []
@@ -31,12 +30,11 @@ const cartsMananger = {
     }
   },
 
-
   getCartById: (req, res) => {
     try {
-      const cartId =Number(req.params.cid);
+      const cartId = Number(req.params.cid);
       const carts = JSON.parse(fs.readFileSync(carritoFilePath, 'utf-8'));
-      const cart = carts.find(cart => cart.id === cartId);
+      const cart = carts.find((cart) => cart.id === cartId);
       if (!cart) {
         return res.status(404).json({ error: 'Carrito no encontrado' });
       }
@@ -45,25 +43,38 @@ const cartsMananger = {
       res.status(500).json({ error: 'Error al obtener el carrito por ID' });
     }
   },
+
   addProductToCart: (req, res) => {
     try {
       const cartId = Number(req.params.cid);
       const productId = Number(req.params.pid);
       const quantity = req.body.quantity || 1;
 
-      let carts = JSON.parse(fs.readFileSync(carritoFilePath, 'utf-8'));
-      let cart = carts.find(cart => cart.id === cartId);
+     
+      const products = JSON.parse(fs.readFileSync(productFilePath, 'utf-8'));
+
+     
+      const product = products.find((product) => product.id === productId);
+      if (!product) {
+        return res.status(404).json({ error: 'Producto no encontrado' });
+      }
+
+      
+      const carts = JSON.parse(fs.readFileSync(carritoFilePath, 'utf-8'));
+      const cart = carts.find((cart) => cart.id === cartId);
       if (!cart) {
         return res.status(404).json({ error: 'Carrito no encontrado' });
       }
 
-      let productInCart = cart.products.find(item => item.product === productId);
+      
+      const productInCart = cart.products.find((item) => item.product === productId);
       if (productInCart) {
         productInCart.quantity += quantity;
       } else {
         cart.products.push({ product: productId, quantity });
       }
 
+     
       fs.writeFileSync(carritoFilePath, JSON.stringify(carts, null, 2));
       res.json(cart);
     } catch (error) {
@@ -72,4 +83,4 @@ const cartsMananger = {
   }
 };
 
-export default cartsMananger;
+export default cartsManager;
