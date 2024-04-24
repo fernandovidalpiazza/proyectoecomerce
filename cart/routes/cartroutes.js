e095bead2071750bd1dbfe7a465a14bbb4145a0e
fernandovidalpiazza <fervidalpiazza@gmail.com>
2024-04-24 14:11:18 -0300

    estoy seguro

--- cart/routes/cartroutes.js
+++ cart/routes/cartroutes.js
@@ -7,5 +7,5 @@ cartsRouter.get('/', cartsController.getAllCarts)
 cartsRouter.post('/', cartsController.createCart);
 cartsRouter.get('/:cid', cartsController.getCartById);
 cartsRouter.post('/:cid/addProduct/:pid', cartsController.addProductToCart);
-
+//estoy seguro del git
 export default cartsRouter;
