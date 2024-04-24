1146a2359cee376fdf68181feb27bb697f91150f
fernandovidalpiazza <fervidalpiazza@gmail.com>
2024-04-24 12:21:10 -0300

    funcioando24

--- app.js
+++ app.js
@@ -1,35 +1,29 @@
-
 import express from "express";
 import productRoutes from "./routes/usersroutes.js"; 
+import cartRoutes from "./cart/routes/cartroutes.js";
 import config from "./config.js";
-import router from "./cart/routes/cartroutes.js";
 
 const app = express();
 
+// Middleware para el manejo de JSON
 app.use(express.json());
 
 // Utiliza las rutas de productos
 app.use("/api/products", productRoutes);
-app.use("/cid/products/:productId", productRoutes); 
+app.use("/api/products/:productId", productRoutes); 
 app.use("/api/addProduct", productRoutes); 
 app.use("/api/updateProduct", productRoutes); 
 app.use("/api/delete", productRoutes);
 
+// Utiliza las rutas del carrito
 
-//crear carrito
-
+app.use('/api/carts', cartRoutes);
 
 
 // Ruta estática para servir archivos estáticos
 app.use('/static', express.static(`${config.DIRNAME}/public`));
 
-// Utiliza las rutas del carrito
-app.use("/api/cart", router);
-app.use("/api/cart/addToCart/:productId", router);
-app.use("/api/cart/removeFromCart/:productId", router);
-
-
-
+// Inicia el servidor
 app.listen(config.PORT, () => {
   console.log(`El servidor está escuchando en el puerto ${config.PORT}`);
 });
--- cart/routes/cartroutes.js
+++ cart/routes/cartroutes.js
@@ -1,52 +1,11 @@
-import { Router } from 'express';
-import CartManager from '../CartManager.js';
+import express from 'express';
+import cartsController from '../cartmanger.js';
 
+const cartsRouter = express.Router();
 
-const router = Router();
-const cartManager = new CartManager();
+cartsRouter.get('/', cartsController.getAllCarts)
+cartsRouter.post('/', cartsController.createCart);
+cartsRouter.get('/:cid', cartsController.getCartById);
+cartsRouter.post('/:cid/addProduct/:pid', cartsController.addProductToCart);
 
-router.post('/:productId', async (req, res) => {
-    try {
-        const productId = req.params.productId;
-        await cartManager.addProductToCart(productId);
-        res.status(200).json({ status: 1, message: "Producto agregado al carrito correctamente" });
-    } catch (error) {
-        console.error("Error al agregar producto al carrito:", error);
-        res.status(500).json({ status: 0, error: "Error al agregar producto al carrito" });
-    }
-});
-
-router.delete('/removeFromCart/:productId', async (req, res) => {
-    try {
-        const productId = req.params.productId;
-        await cartManager.removeFromCart(productId);
-        res.status(200).json({ status: 1, message: "Producto eliminado del carrito correctamente" });
-    } catch (error) {
-        console.error("Error al eliminar producto del carrito:", error);
-        res.status(500).json({ status: 0, error: "Error al eliminar producto del carrito" });
-    }
-});
-
-router.get('/', async (req, res) => {
-    try {
-
-        const cart = await cartManager.getCart();
-        console.log("Contenido del carrito:", cart); // Agregar console.log aquí
-        res.status(200).json({ status: 1, cart });
-    } catch (error) {
-        console.error("Error al obtener el carrito:", error);
-        res.status(500).json({ status: 0, error: "Error al obtener el carrito" });
-    }
-});
-
-router.post('/createCart', async (req, res) => {
-    try {
-        await cartManager.createCart();
-        res.status(200).json({ status: 1, message: "Carrito creado correctamente" });
-    } catch (error) {
-        console.error("Error al crear el carrito:", error);
-        res.status(500).json({ status: 0, error: "Error al crear el carrito" });
-    }
-});
-
-export default router;
+export default cartsRouter;
